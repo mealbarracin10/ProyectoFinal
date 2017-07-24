12539d3889f5550282f732522382eafb7c2f0113
juanshbta69 <juan.shbta69@gmail.com>
2017-07-23 20:03:21 -0500

    Eliminación de código en comentarios

--- src/co/edu/uniandes/graficador/GraficadorResultados.java
+++ src/co/edu/uniandes/graficador/GraficadorResultados.java
@@ -13,58 +13,6 @@ public class GraficadorResultados {
 	private static final String TITULO_GRAFICA_TIEMPOS_CARGA_MODELO = "Carga de modelo";
 	private static final String TITULO_GRAFICA_TIEMPOS_PROCESAMIENTO_QUERY = "Procesamiento Query";
 
-	/*public static void generarGraficaResultadosModeloRDF(int numeroIncremento) {
-		Iterator<Entry<String, List<Long>>> iterador = ManejadorModelos.getEstadisticasModelos().entrySet().iterator();
-		Grafica grafica = new Grafica(TITULO_APLICACION_GRAFICAS);
-
-		while (iterador.hasNext()) {
-			Map.Entry<String, List<Long>> entrada = (Entry<String, List<Long>>) iterador.next();
-			List<Long> valorEntrada = (List<Long>) entrada.getValue();
-
-			switch (entrada.getKey()) {
-			case ManejadorModelos.LLAVE_TIEMPOS_CARGA_MODELO_RDF:
-				grafica.añadirPanelGrafica(LABEL_CONVENCION_MODELO_RDF, TITULO_GRAFICA_TIEMPOS_CARGA_MODELO,
-						valorEntrada, numeroIncremento, BorderLayout.WEST);
-				break;
-			case ManejadorModelos.LLAVE_TIEMPOS_EJECUCION_QUERY_MODELO_RDF:
-				grafica.añadirPanelGrafica(LABEL_CONVENCION_MODELO_RDF, TITULO_GRAFICA_TIEMPOS_PROCESAMIENTO_QUERY,
-						valorEntrada, numeroIncremento, BorderLayout.EAST);
-				break;
-			default:
-				break;
-			}
-
-		}
-
-		grafica.pack();
-		grafica.setVisible(true);
-	}
-
-	public static void generarGraficaResultadosModeloOWL(int numeroIncremento) {
-		Iterator<Entry<String, List<Long>>> iterador = ManejadorModelos.getEstadisticasModelos().entrySet().iterator();
-
-		Grafica grafica = new Grafica(TITULO_APLICACION_GRAFICAS);
-
-		while (iterador.hasNext()) {
-			Map.Entry<String, List<Long>> entrada = (Entry<String, List<Long>>) iterador.next();
-			List<Long> valorEntrada = (List<Long>) entrada.getValue();
-
-			switch (entrada.getKey()) {
-			case ManejadorModelos.LLAVE_TIEMPOS_CARGA_MODELO_OWL:
-				grafica.añadirPanelGrafica(LABEL_CONVENCION_MODELO_OWL, TITULO_GRAFICA_TIEMPOS_CARGA_MODELO,
-						valorEntrada, numeroIncremento, BorderLayout.WEST);
-				break;
-			case ManejadorModelos.LLAVE_TIEMPOS_EJECUCION_QUERY_MODELO_OWL:
-				grafica.añadirPanelGrafica(LABEL_CONVENCION_MODELO_OWL, TITULO_GRAFICA_TIEMPOS_PROCESAMIENTO_QUERY,
-						valorEntrada, numeroIncremento, BorderLayout.EAST);
-			default:
-				break;
-			}
-		}
-		grafica.pack();
-		grafica.setVisible(true);
-	}*/
-
 	/**
 	 * 
 	 * @param numeroIncremento
